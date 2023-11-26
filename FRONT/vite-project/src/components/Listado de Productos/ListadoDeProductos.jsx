dafc2c4ab43a43a21f61b46fd9f8743fb99c6dac
Valen <valenpagli2012@gmail.com>
2023-11-26 18:57:03 -0300

    union back y front

--- FRONT/vite-project/src/components/Listado de Productos/ListadoDeProductos.jsx
+++ FRONT/vite-project/src/components/Listado de Productos/ListadoDeProductos.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import DetalleDeProducto from '../Detalle de Producto/DetalleDeProducto'
-
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 
 function ListadoDeProductos(props) {
@@ -11,9 +12,31 @@ function ListadoDeProductos(props) {
 
   return (
     <div>
-        {console.log(libros)}
+         <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        {libros.map((libro) => {
+          const [categoria] = libro.Categoria;
+          const {nombre} =categoria; 
 
-        {libros.map((libro)=>{
+          return (
+          <Grid key={libro.id} item xs={12} sm={6} md={4} lg={3}>
+            <DetalleDeProducto 
+           
+            id = {libro.id}
+            url_imagen = {libro.url_imagen}
+            titulo = {libro.titulo}
+            autor={libro.autor}
+            precio_$={libro.precio_$}
+            editorial={libro.editorial}
+            categoria={nombre}
+            sinopsis={libro.descripcion}
+            />
+          </Grid>
+        )})}
+      </Grid>
+    </Box>
+
+        {/* {libros.map((libro)=>{
           const [categoria] = libro.Categoria;
           const {nombre} =categoria; 
           
@@ -34,7 +57,7 @@ function ListadoDeProductos(props) {
             />
             );
 
-        })}
+        })} */}
     </div>
   )
 }
